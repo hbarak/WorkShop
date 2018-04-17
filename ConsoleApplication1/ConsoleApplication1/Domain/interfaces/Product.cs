99b698d657de9bb21870488b215c9ea3ab327c76
hbarak <hbarakosh@gmail.com>
2018-04-17 15:01:13 +0300

    first work, added abstract classes and unterfaces for basic objects

---
+++ ConsoleApplication1/ConsoleApplication1/Domain/interfaces/Product.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WorkShop_github
+{
+    abstract class Product
+    {
+        String name;
+        public abstract Discount getAvailableDiscount();
+        public abstract void viewItem();
+        public abstract void buyItem();
+        public abstract bool purchasePolicycheak();
+        public abstract void productPolicy(int min, int max);
+        public abstract void productDiscount(details);          // TODO: decide what are 'detalis'
+    }
+}
---
+++ ConsoleApplication1/WorkShop_github/Domain/interfaces/Store.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WorkShop_github
+{
+    abstract class Store
+    {
+        String id;
+        String name;
+        List<Product> availableProducts;                        //all products for sale
+        public abstract int addProduct(Product product, int quantity);
+        public abstract int removeProduct(Product product, int quantity);
+        public abstract int editProduct(Product product, int quantity);
+        public abstract bool addManager(User user);
+        public abstract bool removeManager(User user);
+        public abstract void viewHistory();
+    }
+}
