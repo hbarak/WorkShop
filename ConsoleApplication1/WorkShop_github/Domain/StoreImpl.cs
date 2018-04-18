634f732272e528b4c90daca589fd385299b42a45
hbarak <hbarakosh@gmail.com>
2018-04-18 15:20:49 +0300

    barak try

--- ConsoleApplication1/WorkShop_github/Domain/StoreImpl.cs
+++ ConsoleApplication1/WorkShop_github/Domain/StoreImpl.cs
@@ -3,14 +3,32 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
 namespace WorkShop_github.Domain
-{
+{ 
     class StoreImpl: Store
     {
-        public StoreImpl(String name)
-        {
+        private static IdGenerator.IdGenerator Idgen;
+        String name;
+        Dictionary<Product, int> availableProducts;                        //all products for sale
 
+        public StoreImpl(String name, String id, Dictionary<Product, int> availableProducts)
+        {
+            this.name = name;
+            this.id = id;
+            this.availableProducts = availableProducts;
+        }
+
+        public static StoreImpl newStore(String name)
+        {
+            return new StoreImpl(name, getId(), new Dictionary<Product, int>());
+        }
+
+
+
+
+        private static String getId()
+        {
+            return Idgen.get();
         }
     }
 }
